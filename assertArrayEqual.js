ad26707eeb5394afb12f87bcdcb09fe38f25a1ea
ar4sdoteth <ashley.barr@ar4s.com>
2020-11-25 14:26:22 -0700

    refactored code in assertArrayEqual to take in two parameters/arrays and return more concise results, added without function and test cases

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,28 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔥 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-let eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1.length !== array2.length) {
-      return false;
+//function takes in two arrays
+const assertArraysEqual = function(array1, array2) {
+  //if the lengths are not equal, return a failed assertion
+  if (array1.length !== array2.length) {
+    return `🔥 Assertion Failed: Arrays have unequal length`;
+  } 
+  //if the values of the equal arrays differ, return a failed assertion
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return `🔥 Assertion Failed: Array has unequal values`;
     }
-    if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
+  }
+  //if the lengths aren't different, and the values are different, the arrays must be the same, return a passed assertion
+  return `🟢 Assertion Passed: Arrays are the same`;
 };
 
-let assertArraysEqual = function(eqArrays) {
-  if (!eqArrays) {
-    return `🔥 Assertion Failed: Arrays Differ`;
-  }
-  if (eqArrays) {
-    return `🟢 Assertion Passed: Arrays are the same`;
-  }
-};
-console.log(assertArraysEqual(eqArrays([1,"2"],[1,"2"])));
+console.log(assertArraysEqual([1,"2", "ten", 99],[1,"2", "ten", 99]));
---
+++ without.js
@@ -0,0 +1,40 @@
+//borrowed code from assertArrayEqual.js 
+//function takes in two arrays
+const assertArraysEqual = function(array1, array2) {
+  //if the lengths are not equal, return a failed assertion
+  if (array1.length !== array2.length) {
+    return `🔥 Assertion Failed: Arrays have unequal length`;
+  } 
+  //if the values of the equal arrays differ, return a failed assertion
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return `🔥 Assertion Failed: Array has unequal values`;
+    }
+  }
+  //if the lengths aren't different, and the values are different, the arrays must be the same, return a passed assertion
+  return `🟢 Assertion Passed: Arrays are the same`;
+};
+//without takes in two arrays and uses the second array -itemsToRemove- to remove items from the first array -source- items from the source array intact
+const without = function(source, itemsToRemove) {
+  //loop through source to collect the array elements
+  for (let item of source) {
+    //if itemsToRemove doesn't include the array elements from source, push the remaining elements to the newArray
+    if (!itemsToRemove.includes(item)) {
+      newArray.push(item);
+    }
+  } 
+  return newArray
+};
+
+
+// --- TEST CASES ---
+
+// console.log(without([1, 2, 1], [1]));
+// withthout(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+// console.log(without(words, ["lighthouse"]));
+// console.log(words);
+console.log(assertArraysEqual(words, ["hello", "world"]));
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouz"]));
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));

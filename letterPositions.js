696db238846727cf2db0110cd69072665b7ea68c
ar4sdoteth <ashley.barr@ar4s.com>
2020-11-25 20:26:35 -0700

    made a function that logs all of the positions of the characters in a string

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(sentence) {
+  const results = {}; //console.log(results)
+  const noSpaceString = sentence.split(" ").join("");
+  for (let i = 0; i < noSpaceString.length; i++) {
+    let letter = noSpaceString[i]; //looking up the letter from the index in the string.
+    if (results[letter]) {
+      results[letter].push(i) //pushes the letter to the key in results
+    } else {
+      results[letter] = [i]; //creates an array from the index
+    }
+  }
+  // logic to update results here
+  return results;
+};
+
+// --- TEST CASES ---
+
+console.log(letterPositions("lighthouse blah blah blah"));

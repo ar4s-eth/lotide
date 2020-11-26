21d5138265c8edc69b137f2ebf29b9ae8b1822bd
ar4sdoteth <ashley.barr@ar4s.com>
2020-11-25 19:35:22 -0700

    added a function using a for of loop to index the letters and return the result.

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//function takes in a string
+const countLetters = function(sentence) {
+  let result = {}
+  sentence = sentence.split(" ").join("")
+  //loop through the sentence array
+  for (let letter of sentence) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else if (result[letter] === " ") {
+      delete result[letter];
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result
+};
+
+// --- TEST CASES ---
+
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("pOlly wAnt A crAckEr?"));

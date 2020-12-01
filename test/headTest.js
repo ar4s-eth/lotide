43dac320831255f2ff2553285f462d3af86a3cd7
ar4sdoteth <ashley.barr@ar4s.com>
2020-11-30 19:25:53 -0700

    added mocha/chai to the test suite and modified headTest to use them

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
 const assertEqual = require('../assertEqual.js')
 const head = require('../head.js')
+const assert = require('chai').assert;
 
-// TEST CODE
- 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([])));
+describe('#head', () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it('returns the number 5 from an array of [5,6,7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+  it("returns 'Hello' from an array of ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+});

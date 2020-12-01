319577f976eaa5bd69f382f9654d9c9d0669eedb
ar4sdoteth <ashley.barr@ar4s.com>
2020-11-30 17:46:44 -0700

    added test file for eqArray, moved test code there

---
+++ test/eqArrayTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual.js')
+const eqArrays = require('../eqArrays.js')
+
+// --- TEST CASES ---
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
